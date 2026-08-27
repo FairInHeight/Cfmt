517c0e2d309e5438be9bc2a073bbae3bf28abe76
Christopher C Taylor <66293653+FairInHeight@users.noreply.github.com>
2026-08-27 09:44:10 -0400

    Refactor dispatch API to return handlers

--- internal/dispatch.h
+++ internal/dispatch.h
@@ -9,21 +9,12 @@ typedef struct
     size_t *index;
 } DispatchContext;
 
-typedef enum
-{
-    DISPATCH_INVALID,
-    DISPATCH_STYLE,
-    DISPATCH_FOREGROUND,
-    DISPATCH_BACKGROUND,
-    DISPATCH_BRIGHT_FOREGROUND,
-    DISPATCH_BRIGHT_BACKGROUND,
-    DISPATCH_RGB
-} DispatchType;
+typedef char *(*DispatchHandler)(DispatchContext *ctx);
 
-DispatchType dispatch(char spec);
+DispatchHandler dispatch(char spec);
 
-char *dispatch_style(char spec);
-char *dispatch_foreground(char spec);
+char *dispatch_style(DispatchContext *ctx);
+char *dispatch_foreground(DispatchContext *ctx);
 char *dispatch_background(DispatchContext *ctx);
 char *dispatch_bright_foreground(DispatchContext *ctx);
 char *dispatch_bright_background(DispatchContext *ctx);
